930e7d2d2565469521e23fee0fb717ef5951bf5c
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-05-19 11:38:57 +0530

    Update couples.py

--- Flare_Robot/modules/couples.py
+++ Flare_Robot/modules/couples.py
@@ -57,7 +57,6 @@ async def couple(_, message):
 __New couple of the day may be chosen at 12AM {tomorrow}__"""
             await pbot.send_message(message.chat.id, text=couple_selection_message)
             couple = {"c1_id": c1_id, "c2_id": c2_id}
-            )
 
         else:
             c1_id = int(is_selected["c1_id"])
