d4def8136c2922515f2e80b7ff4c5d44dd9c0911
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-05-26 13:42:51 +0530

    Update promote\demote.py

--- Flare_Robot/modules/promote\demote.py
+++ Flare_Robot/modules/promote\demote.py
@@ -140,7 +140,7 @@ def ppromote(update: Update, context: CallbackContext) -> str:
 
     TEXT = "#SUCCESSFULLY PROMOTED"
 
-    await bot.send_file(event.chat_id, caption=TEXT, buttons=BUTTON)
+    bot.send_message(event.chat_id, caption=TEXT, buttons=BUTTON)
 
 
     log_message = (
