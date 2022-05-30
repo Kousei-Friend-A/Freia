22e9c7b6e510550903610dade5def37d8bf3fcbd
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-05-30 14:25:33 +0530

    Update userinfo.py

--- Flare_Robot/modules/userinfo.py
+++ Flare_Robot/modules/userinfo.py
@@ -32,6 +32,7 @@ from Flare_Robot import (
     WOLVES,
     dispatcher,
     sw,
+    StartTime
 )
 from Flare_Robot import telethn as FlareTelethonClient
 from Flare_Robot.__main__ import STATS, TOKEN, USER_INFO
@@ -484,6 +485,18 @@ BUTTON = [
 def stats(update, context):
     uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
     status = "*╒═══「 System Statistics 」*\n\n"
+    status += "*➢ System Start time:* " + str(uptime) + "\n"
+    uname = platform.uname()
+    status += "*➢ System:* " + str(uname.system) + "\n"
+    status += "*➢ Node name:* " + escape_markdown(str(uname.node)) + "\n"
+    status += "*➢ Release:* " + escape_markdown(str(uname.release)) + "\n"
+    status += "*➢ Machine:* " + escape_markdown(str(uname.machine)) + "\n"
+    mem = virtual_memory()
+    cpu = cpu_percent()
+    disk = disk_usage("/")
+    status += "*➢ CPU:* " + str(cpu) + " %\n"
+    status += "*➢ RAM:* " + str(mem[2]) + " %\n"
+    status += "*➢ Storage:* " + str(disk[3]) + " %\n\n"
     status += "*➢ Python Version:* " + python_version() + "\n"
     status += "*➢ python-Telegram-Bot:* " + str(ptbver) + "\n"
     try:
