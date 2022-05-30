0715948a3b06e09f9d5aceee4e82a8929728563e
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-05-30 10:52:50 +0530

    Update misc.py

--- Flare_Robot/modules/helper_funcs/misc.py
+++ Flare_Robot/modules/helper_funcs/misc.py
@@ -69,7 +69,7 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if calc in [1, 2]:
         pairs.append((modules[-1],))
     else:
-        pairs += [[EqInlineKeyboardButton("[► Back ◄]", callback_data="hottie_back")]]
+        pairs += [[EqInlineKeyboardButton("[► Back ◄]", callback_data="help_back")]]
 
     return pairs
 
