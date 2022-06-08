f81ab208675ad2b7db879bd6d7a2091ad0fa6ec0
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-06-08 12:15:53 +0530

    Update status.py

--- Flare_Robot/modules/status.py
+++ Flare_Robot/modules/status.py
@@ -65,6 +65,8 @@ async def awake(event):
     status += "*➢ Storage:* " + str(disk[3]) + " %\n\n"
     status += "*➢ Python Version:* " + python_version() + "\n"
     status += "*➢ python-Telegram-Bot:* " + str(ptbver) + "\n"
+    status += "\n*Bot statistics*:\n"
+    status += ""\n".join([mod.__stats__() for mod in STATS])"
     BUTTON = [
         [
             Button.url("📢 Updates", "https://t.me/Freia_Updates"),
