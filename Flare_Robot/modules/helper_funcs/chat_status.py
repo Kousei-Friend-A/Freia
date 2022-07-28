4fced5a1fa84287813988fda2c746bcfc14f2fc6
Asta-silva <89698535+Asta-silva@users.noreply.github.com>
2022-07-28 09:23:15 +0530

    Update chat_status.py

--- Flare_Robot/modules/helper_funcs/chat_status.py
+++ Flare_Robot/modules/helper_funcs/chat_status.py
@@ -426,6 +426,7 @@ def connection_status(func):
             update,
             update.effective_chat,
             update.effective_user.id,
+            null=True,
             need_admin=False,
         )
 
